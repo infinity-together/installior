f1051570494ff76e72d4c7d4ebad12dfac583b3c
Infinity Together <65680280+JustCorporation@users.noreply.github.com>
2021-05-01 18:39:40 +0500

    0.0.1

---
+++ Installior/Installior/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Installior.View;
+
+namespace Installior
+{
+    public partial class MainWindow : Window
+    {
+        public static MainWindow Components { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Components = this;
+        }
+
+
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            PagePlace.Content = new StartPage();
+        }
+
+        private void ToolBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ToolBar_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Installior/Installior/View/StartPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Installior.View
+{
+    /// <summary>
+    /// Логика взаимодействия для StartPage.xaml
+    /// </summary>
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NextStep_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Components.PagePlace.Content = new InstallationChoosePage();
+        }
+    }
+}
